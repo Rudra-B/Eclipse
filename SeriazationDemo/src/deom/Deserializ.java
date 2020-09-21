1bbf9ad54e6c2b739a473bd594276030d7fd8449
Biradar <rudrappa.biradar@capgemini.com>
2020-09-21 18:17:31 +0530

    desetialization completed

---
+++ SeriazationDemo/src/deom/Deserializ.java
@@ -0,0 +1,25 @@
+package deom;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class Deserializ {
+
+	public static void main(String[] args) {
+		
+		
+		try(FileInputStream fs=new FileInputStream("zak.txt");
+				ObjectInputStream oi=new ObjectInputStream(fs))
+		{
+			Model md=(Model)oi.readObject();
+			
+			System.out.println(md);
+		}catch(IOException |ClassNotFoundException e)
+		{
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
--- SeriazationDemo/src/deom/Model.java
+++ SeriazationDemo/src/deom/Model.java
@@ -9,7 +9,7 @@ public class Model implements Serializable {
 	 */
 	private static final long serialVersionUID = 4994409606841904705L;
 	private String name;
-	private String password;
+	private transient  String password;
 	private String number;
 	public Model(String name, String password, String number) {
 		
